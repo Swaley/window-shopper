7a760ddd60f9b69fee86882b09b2fc152318db7b
Swaley Palmer <swaley1980@swaleys-macbook-pro.local>
2018-02-21 18:09:02 +0000

    "calculateButton

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcButton.backgroundColor = #colorLiteral(red: 1, green: 0.7133382382, blue: 0.005135498657, alpha: 1)
+        calcButton.setTitle("Calculate", for: .normal)
+        calcButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcButton.addTarget(self, action: #selector(MainVC.calculate) , for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcButton
+        priceTxt.inputAccessoryView = calcButton
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func calculate() {
+        print("we got here")
     }
-
 
 }
 
